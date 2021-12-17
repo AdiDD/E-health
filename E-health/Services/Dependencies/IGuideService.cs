995b6067472c01f18f411eb9870650e78c171624
red-victor <77648244+red-victor@users.noreply.github.com>
2021-12-17 14:43:37 +0200

    Add dependencies

---
+++ E-health/Services/Dependencies/IGuideService.cs
@@ -0,0 +1,12 @@
+﻿using E_health.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace E_health.Services.Dependencies
+{
+    public interface IGuideService: IService<Guide>
+    {
+    }
+}
